12a78f1363e5ef3ee68b1a851edf86809041bfc2
Rahul Jain <rahuldkjain@gmail.com>
2018-03-20 14:50:21 +0530

    ..

--- placement/admin_process.php
+++ placement/admin_process.php
@@ -5,16 +5,16 @@
     $_SESSION["auth"]=0;
     $user=$_POST["user"];
     $pass=$_POST["pass"];
-    $query="SELECT rollno,name,AES_DECRYPT(password,'iiitdmjp_password') as 'pass' FROM students WHERE rollno='$user'";
+    $query="SELECT admin_id,admin_name,AES_DECRYPT(password,'iiitdmjp_password') as 'pass' FROM admin WHERE rollno='$user'";
     $res=mysqli_query($con,$query);
     if($res){
 
     while($a=mysqli_fetch_assoc($res)){
         if($a["pass"]==$pass){
             $_SESSION["auth"]=1;
-            $_SESSION["name"]=$a["name"];
-            $_SESSION["rollno"]=$a["rollno"];
-            header("Location: student_login.php");
+            $_SESSION["name"]=$a["admin_name"];
+          
+            header("Location: admin_panel.php");
             exit();
         }
         else{
