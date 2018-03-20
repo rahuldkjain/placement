7d9e896436de6e4f34802425685886a0aa5ab0d0
Rahul Jain <rahuldkjain@gmail.com>
2018-03-20 14:26:53 +0530

    connection added

---
+++ placement/connection.php
@@ -0,0 +1,7 @@
+<?php
+$con=mysqli_connect("136.243.67.94","iiitdmjp","42ka3L8yaUF343q","iiitdmjp_new");
+if(mysqli_connect_errno()){
+    echo 'Failed to connect to MySQL: '.mysqli_connect_error();
+}
+
+?>
